0eb90517c0a13a7685c486a1a59a30045f0def71
guidogr95 <guido.gr@hotmail.com>
2021-08-31 10:44:09 -0500

    first commit

---
+++ .storybook/yourTheme.js
@@ -0,0 +1,16 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+
+  // UI
+  appBg: 'white',
+  appContentBg: 'white',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+  brandTitle: 'DS Pichincha',
+  brandImage: 'https://logodownload.org/wp-content/uploads/2020/04/banco-pichincha-logo-4.png',
+});
---
+++ src/models/boxItem.tsx
@@ -0,0 +1,9 @@
+import { names } from '../components/atoms/icon/names'
+
+export interface BoxItem {
+  idelement: string
+  titleitem: string
+  caption?: string
+  number?: string
+  icon?: names
+}
