4c9caaa270d4fe72e5770d6d2b655906d53b973a
guidogr95 <guido.gr@hotmail.com>
2021-08-31 10:50:41 -0500

    new change

--- src/models/boxItem.tsx
+++
@@ -1,9 +0,0 @@
-import { names } from '../components/atoms/icon/names'
-
-export interface BoxItem {
-  idelement: string
-  titleitem: string
-  caption?: string
-  number?: string
-  icon?: names
-}
